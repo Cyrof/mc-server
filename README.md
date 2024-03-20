19d0df8fd0bf85906248e7918eed77de192598a1
keith <keithneo00@gmail.com>
2024-03-20 21:42:32 +0800

    created backup script

--- README.md
+++ README.md
@@ -1,11 +1,62 @@
-# Minecraft Server
-This is a minecraft server using minecraft docker image, docker compose, and ngrok for tcp connection without port forwarding
+# Minecraft Server with Docker and Backup Script
+This repository hosts a Minecraft server setup utilizing Docker and Docker Compose, providing flexibility for deployment with or without port forwarding. Additionally, It includes a backup script to automate the process of backing up Minecraft server data.
 
-## Instructions
-1. Update `NGROK_AUTHTOKEN` to your own ngrok authentication token
-2. Navigate to the current directory  
-3. Run `sudo docker compose up -d`
-4. Check if ngrok is up using `sudo docker ps`. If container not up open a github issue. 
-5. Navigate to localhost:4040 on a browser to get the tcp address.
-6. Copy the tcp address without the `tcp://`
-7. In minecraft connect to server using the address copied.
+## Docker Setup
+### Docker Compose Files
+- **docker-compose-ngrok.yaml**: This file sets up the Minecraft server along with an Ngrok container, allowing for for easy access to the server without the need for port forwarding. The Minecraft container is connected to the same Ngrok container, ensuring seamless integration. <br>
+**IMPORTANT** Change the `<ngrok authtoken>` to your own ngrok token
+``` yaml
+environment:
+      - NGROK_AUTHTOKEN=<ngrok authtoken>
+```
+
+- **docker-compose-pf.yaml**: This file sets up the Minecraft server without Ngrok. Users are required to port forward the default Minecraft port (25565) on their router for external access.
+
+### Usage
+To start the server, simply run: 
+``` bash
+$ docker-compose -f <docker-compose-file> up -d
+```
+If you preder using Docker directly for port forwarding, you can use the following command:
+```bash
+$ docker run -d -p 25565:25565 -e EULA=TRUE -v <server_path>:/data itzg/minecraft-server
+```
+**IMPORTANT** change the <server_path> to the path of your minecraft server to link to docker container. Please ensure that you have port 25565 forwarded on your router for external access to the server.
+
+### Backup Script
+The '**`mc-backup-script`**' folder contains a Python script '**`backup.py`**', designed to automate the backup process for your Minecraft server data.
+
+### Setup 
+1. Navigate into the folder:
+    ```bash
+    $ cd mc-backup-script
+    ```
+2. Create a virtual environment:
+    ```bash
+    $ python3 -m venv <name_of_venv>
+    ``` 
+3. Activate the virtual environment:
+    ```bash
+    $ source <name_of_venv>/bin/activate
+    ```
+4. Install the required packgages:
+    ```bash 
+    $ pip install -r requirements.txt
+    ```
+5. Create a '**`.env`**' file in the '**`mc-backup-script`**' folder.
+6. Add the following variables to the '**`.env`**' file:
+   ```plaintext
+   SRC=<path-to-mc-server-folder>
+   DST=<path-to-backup-folder>
+   ```
+   Replace '**`<path-to-mc-server-folder>`**' with the full path to your Minecraft server folder containing the data you want to back up, and '**`<path-to-backup-folder>`**' with the full path where you want to store your backups.
+
+### Usage
+Set up cronjob with 5 min interval to run this python script.
+
+## Contributing 
+Feel free to contribute to this project by forking this repository and creating a pull request with your changes.
+
+## Issues
+If you encounter any issues or have suggestions for improvements, please open an issue in the repository.
+
---
+++ mc-backup-script/backup.py
@@ -0,0 +1,105 @@
+from datetime import datetime, timedelta
+import os 
+from dotenv import load_dotenv
+import shutil
+
+def get_env_var():
+    """
+    Fucntions to retrieve environment variables.
+
+    This function retrieves environment variables 'SRC' and 'DST' using os.getenv().
+
+    Returns: 
+        dict: A dictionary containing environment variables 'SRC' and 'DST'.
+    """
+    keys = ["SRC", "DST"]
+    env_var = {key: os.getenv(key) for key in keys} # Retrieving environment variables
+
+    return env_var
+
+def zipFolder(folderName):
+    """
+    Function to zip a folder.
+
+    Args:
+        folderName (str): Name of the folder to be zipped.
+    """
+    try:
+        shutil.make_archive(f"{folderName}", "zip", folderName) # Creating a zip archieve of the folder
+    except Exception as e:
+        print("An error occured\n", e) # Handling errors
+
+
+def createBackup(env_var, now):
+    """
+    Function to create backups of a source folder.
+
+    This function creates up to three backups of the source folder specified in the environment variable 'SRC'.
+    Each backup is copied to a destination folder specified in the environment variable 'DST'.
+    The backups are named with a timestamp to indicate when they were created.
+
+    Args:
+        env_var (dict): A dictionary containing environment variables 'SRC' and 'DST'.
+        now (datetime): Current date and time.
+    """
+    dt_string = now.strftime("%d-%m-%Y_%H:%M:%S") # Formatting current datetime
+
+    dest_path = env_var["DST"]
+    for i in range(1,4):
+        backup_folder = f"{dest_path}/mc_server_backup_{i}_{dt_string}" # Naming the backup folder
+        try:
+            shutil.copytree(env_var["SRC"], backup_folder) # Copying the source folder to the backup folder
+            zipFolder(backup_folder) # Zipping the backup folder
+            print("Backup zipped")
+            shutil.rmtree(backup_folder) # Removing the backup folder
+        except Exception as e:
+            print("An error occured\n", e) # Handling errors
+
+def old_file(filename, now):
+    """
+    Function to check if a file is older than 10 minutes.
+
+    This function extracts the timestamp from the filename and compares it with the currenttime.
+    If the time difference is greater than 10 minutes, it returns True.
+
+    Args:
+        filename (str): Name of the file.
+        now (datetime): Current date and time.
+
+    Returns:
+        bool: True if the file is older than 10 minutes, False otherwise.
+    """
+    removeExt = filename.split('.')[0].split('_')
+    time = "_".join(removeExt[-2:])
+    file_time = datetime.strptime(time, '%d-%m-%Y_%H:%M:%S') # Extracting timestamp from filename
+
+    time_diff = now - file_time # Calculating time difference
+    ten_min = timedelta(minutes=10)
+    return time_diff > ten_min # Checking if time difference is greater than 10 minutes
+
+def houseKeep(env_var, now):
+    """
+    Function to perform housekeeping by removing old backup files.
+
+    This function iterates through the files in the destination folder specified in the environment variable 'DST'.
+    It checks each file's timestamp to determine if it is older than 10 minutes.
+    If a file is older than 10 minutes, it removes the file.
+
+    Args:
+        env_var (dict): A dictionary containing environment variables 'DST'.
+        now (datetime): Current date and time.
+    """
+    for file in os.listdir(env_var["DST"]):
+        if old_file(file, now): # Checking if the file is older than 10 minutes.
+            os.remove(f"{env_var['DST']}/{file}") # Removing the old backup file
+        
+    
+
+if __name__ == "__main__":
+    load_dotenv() # loading environment variables form .env file
+    data = get_env_var() # Retrieving environment variables
+
+    now = datetime.now() # Getting current date and time 
+
+    createBackup(data, now) # Creating backups
+    houseKeep(data, now) # Performing housekeeping 
